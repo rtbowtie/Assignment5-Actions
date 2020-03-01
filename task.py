739154988eabc78651764a0efd9729d5669a4a30
Randy Tuucker <tuckerra@oregonstate.edu>
2020-02-29 21:36:20 -0500

    Add Area Function And Test

--- task.py
+++ task.py
@@ -1,3 +1,10 @@
+import math
 
 def firstrun():
     return "success"
+
+def area(radius):
+    pi = math.pi
+    mid = pow(radius, 2)
+    area = pi*mid
+    return area
--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import unittest
 import task
-
+import random
+import math
 
 class TestCase(unittest.TestCase):
 
@@ -11,6 +12,11 @@ class TestCase(unittest.TestCase):
     def test2(self):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
+
+    def test3(self):
+        randNum = random.radom()
+        oracle = (math.pi)*((randNum)*randNum)
+        pred = task.area(randNum)
 
 
 if __name__ == '__main__':
