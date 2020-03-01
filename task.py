c6105afb5d8a7f6413fd4daf888a6e108fd1eb42
Randy Tuucker <tuckerra@oregonstate.edu>
2020-02-29 22:46:48 -0500

    Update task.py

--- task.py
+++ task.py
@@ -22,6 +22,6 @@ def listFunc(list):
 
 
 def duration(date1, date2):
-    d1 = datetime.strptime(date1, "%Y-%m-%d")
-    d2 = datetime.strptime(date2, "%Y-%m-%d")
+    d1 = datetime.strptime(str(date1), "%Y-%m-%d")
+    d2 = datetime.strptime(str(date2), "%Y-%m-%d")
     return abs((d2 - d1).days)
